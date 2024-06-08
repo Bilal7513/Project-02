ef0e74bc93a7677c8e167d2b63b7e6ba059423a0
Bilal Irfan <bi0755562@gmail.com>
2024-06-08 23:26:39 +0500

    Project 2
    JavaScript Complete

--- script.js
+++ script.js
@@ -5,15 +5,39 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieselect = document.getElementById('movie');
 
+populateUI();
+
 let ticketprice = +movieselect.value; 
 
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
     const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
-    console.log(seatsIndex);
     const selectedSeatsCount = selectedSeats.length;
     count.innerText = selectedSeatsCount;
     total.innerText = selectedSeatsCount * ticketprice;
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+}
+
+// Save the movie data to local storage
+function setMovieData(movieIndex, moviePrice){
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
+// Get data from local storage and populateUI
+function populateUI(){
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+    if(selectedSeats !== null && selectedSeats.length > 0){
+        seats.forEach((seat, index) => {
+            if(selectedSeats.indexOf(index) > -1){
+                seat.classList.add('selected');
+            }
+        })
+    };
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+    if(selectedMovieIndex !== null){
+        movieselect.selectedIndex = selectedMovieIndex;
+    }
 }
 
 // Event Listner
@@ -29,4 +53,8 @@ container.addEventListener('click', e => {
 movieselect.addEventListener('change', e => {
     ticketprice = +e.target.value;
     updateSelectedCount();
+    setMovieData(e.target.selectedIndex, e.target.value);
 })
+
+// Initial count and total price
+updateSelectedCount();
